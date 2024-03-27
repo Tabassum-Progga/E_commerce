cc32ce1e9755855db2cfb932d5c5d7ff9e508cd8
nuzhatprogga <nuzhatprogga76@gmail.com>
2024-03-27 19:58:18 +0600

    add data

--- ProductsManagement/models.py
+++ ProductsManagement/models.py
@@ -21,6 +21,7 @@ class Product(models.Model):
     price = models.IntegerField(blank=True, null=True)
     category = models.CharField(max_length=200,blank=True, null=True)
     description = models.TextField(blank=True, null=True)
+    image = models.ImageField(blank=True, null=True)
 
     def __str__(self):
         return self.name
--- e_com/urls.py
+++ e_com/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
 from ProductsManagement import views as p_views
 
 urlpatterns = [
@@ -23,3 +25,5 @@ urlpatterns = [
     path('', p_views.products, name = 'products'),
     path('orders/',p_views.orders, name = 'orders'),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
